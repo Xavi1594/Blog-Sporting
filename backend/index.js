e9d035bbf87c55ec0e73961badde2dd37c782ee5
Xavi1594 <xavi9415@hotmail.com>
2023-06-28 09:47:22 +0200

    metodo get en el back

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,53 @@
-const port = process.env.PORT || 3000;
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
-const mysql = require("mysql2");
 const cors = require("cors");
+const db = require("./dbConecction"); 
+
+
 const app = express();
-require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: "http://localhost:3001",
-    credentials: true,
-  };
-  app.use(cors(corsOptions));
-  
-  
-  const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  
-  
-  
-  db.connect((err) => {
-    if (err) throw err;
-    console.log("Conexión establecida con la base de datos");
-  });
-  app.use(express.static("public"));
-  
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  origin: "http://localhost:3001",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-  app.use(
-    session({
-      secret: process.env.JWT_SECRET || "my-secret-key",
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
-  
-  
-app.listen(port, () => {
-    console.log(`Servidor iniciado en el puerto ${port}`);
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: process.env.JWT_SECRET || "my-secret-key",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+
+app.get("/posts",  (req, res) => {
+    db.query(
+      "SELECT id, img_post, titulo_post, fecha_post, contenido_post  FROM posts ",
+      (err, results) => {
+        if (err) {
+          console.error("Error al obtener los posts:", err);
+          res.status(500).json({
+            message:
+              "Ha ocurrido un error al obtener los posts. Por favor, intenta más tarde.",
+          });
+          return;
+        }
+        res.json(results);
+      }
+    );
   });
+  
+
+
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+});
